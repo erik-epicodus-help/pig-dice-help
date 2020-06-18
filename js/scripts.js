fdb264d26dbc3a6ab4dac2b636759272b05cb688
Matthew Craig <mgcraig78@gmail.com>
2020-06-18 13:55:37 -0700

    hide player buttons, add html styling
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//if buttonHold2 click || turnRoll === 1 => switch activeif buttonHold1 click || turnRoll === 1 => switch active
+
 // Biz Logic
 
 function Player(currentRoll, turnScore, totalScore, isActive) {
@@ -24,6 +26,20 @@ Player.prototype.gameScore = function(){
     } else { return this.totalScore 
 };
 };
+
+function activePlayer () {
+  if (player1.isActive === true) {
+    $(".btn-holdTwo").hide();
+    $(".btn-rollTwo").hide();
+    $(".btn-holdOne").show()
+    $(".btn-rollOne").show()
+  } else {
+    $(".btn-holdOne").hide()
+    $(".btn-rollOne").hide()
+    $(".btn-holdTwo").show();
+    $(".btn-rollTwo").show();
+  }
+}
 // UI Logic
 
 let player1 = new Player();
@@ -35,45 +51,50 @@ let btnRollTwo = document.querySelector('.btn-rollTwo');
 let btnHoldTwo = document.querySelector('.btn-holdTwo');
 
 $(document).ready(function() {
+  $("#show-rules").click(function() {
+    $("#rules").toggle();
+  });
+
   $(".btn-rollOne").click(function(event) {
     event.preventDefault();
+    player1.isActive = true
+    activePlayer();
     player1.rollDice();
   $('.player-1-roll').text(player1.currentRoll)
   $('.player-1-turn-total').text(player1.turnScore)
-  })
-
-  $(".btn-rollTwo").click(function(event) {
-    event.preventDefault();
-    player2.rollDice();
-  $('.player-2-roll').text(player2.currentRoll)
-  $('.player-2-turn-total').text(player2.turnScore)
   })
 
   $(".btn-holdOne").click(function(event) {
     event.preventDefault();
     player1.isActive = false
     player2.isActive = true
+    activePlayer();
     player1.gameScore();
     player1.currentRoll = 0
     player1.turnScore = 0
-    $('.player-1-game-score').text(player1.gameScore)
+    $('.player-1-game-score').text(player1.totalScore)
 
+
+  $(".btn-rollTwo").click(function(event) {
+    event.preventDefault();
+    activePlayer();
+    player2.rollDice();
+  $('.player-2-roll').text(player2.currentRoll)
+  $('.player-2-turn-total').text(player2.turnScore)
+  })
 
   })
 
   $(".btn-holdTwo").click(function(event) {
     event.preventDefault();
+    console.log(player1.isActive)
     player1.isActive = true
     player2.isActive = false
+    console.log(player1.isActive)
+
     player2.gameScore();
     player2.currentRoll = 0
     player2.turnScore = 0
-    $('.player-2-game-score').text(player2.gameScore)
-
-
-  })
+    $('.player-2-game-score').text(player2.totalScore)
+  });
 });
-
-
-// if buttonHold2 click || turnRoll === 1 => switch active
-// if buttonHold1 click || turnRoll === 1 => switch active
